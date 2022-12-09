b6206e2f299a5965ea8522592b11a7337e2f2704
Henry Owens <henry.tom.owens@gmail.com>
2022-12-09 15:50:07 +0100

    Removing hard coded transisition

--- src/components/elements/styles/img.ts
+++ src/components/elements/styles/img.ts
@@ -3,7 +3,6 @@ import { style } from "typestyle";
 
 export default (margin: Spacings, width: string, height: string) => {
   return style({
-    transition: "3s",
     width,
     height,
     ...useMargin(margin),
